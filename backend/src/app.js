4a8d461da97aa536bcd71b74f86a89c1ad40e0e8
Vallabh Sangvikar <vallabhdsangvikar18@gmail.com>
2024-08-04 16:54:59 +0530

    starting with the project

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,7 @@ import express from "express";
 const app=express();
 const port=3000;
 
-const nums=18;
-app.get("/api/raj",(req,res)=>{
-    res.json({nums:nums});
-})
+
 
 app.listen(port,()=>{
     console.log(`running on ${port}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,7 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 function App() {
-  let first=10;
-  const [number, setNumber] = useState(null);
-  useEffect(() => {
-    axios.get("/api/raj")
-      .then((res) => {
-        setNumber(res.data.nums);
-        console.log(res.data.nums);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }, []);
-
-    return (<>
-    Hey Raj Parikh <h1>{number !== null ? number : 'Loading...'}</h1>    </>)  
+    return  
 }
 
 export default App
