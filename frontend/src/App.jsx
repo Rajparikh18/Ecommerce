91f1c171800163a318bf5fd7b21632b7a9e9c092
Vallabh Sangvikar <vallabhdsangvikar18@gmail.com>
2024-08-05 20:50:59 +0530

    created main content on home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Slider from './components/Slider'
+import image1 from './assets/slider1.jpg';
 import './App.css'
 
+const slides = [
+    {
+        mainImage: image1,
+        title: 'Slide 1',
+        subtitle: 'Subtitle 1',
+        description: 'Description 1',
+    },
+    {
+        mainImage: image1,
+        title: 'Slide 2',
+        subtitle: 'Subtitle 2',
+        description: 'Description 2',
+    },
+];
 
 function App() {
     return (
       <>
         <Header></Header>
         <Navbar></Navbar>
+        <Slider slides={slides}></Slider>
         <Footer></Footer>
       </>
     )
