9a5c79986d009283d9640592cb8202b17b97b895
Vallabh Sangvikar <vallabhdsangvikar18@gmail.com>
2024-08-12 19:58:28 +0530

    minor css changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Home from './pages/Home'
 import demoimage from './assets/images/demoimage.jpeg'
-import Navbar from './components/Navbar';
 import ProductPage from './pages/Productpage';
 
 function App() {
