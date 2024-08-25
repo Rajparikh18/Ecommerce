eb8b0839ae2b827e99aab6d8e782f431288cfbc9
Rajparikh18 <parikhraj85@gmail.com>
2024-08-25 10:22:34 +0530

    jwt token through authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,73 +23,8 @@ import { User } from './models/user.model.js';
 
 
 //routes declaration 
-app.use("/users",userRouter);
-
-
-// Signup 
-app.post("/register",async(req,res)=>{
-    try{
-        const {username,email,password}=req.body;
-        if(!(username && email && password)){
-            res.status(400).send("All fields are compulsory");
-        }
-        // Checking if user already exist
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            res.status(401).send('User already exist with this email');
-        }
-        const user=new User({
-            username,email,password
-        })
-        await user.save();
-
-        const token=user.generateAccessToken();
-        user.token=token;
-        user.password=undefined;
-
-        res.status(201).json(user);
-
-
-    }catch(err){
-        console.log(err);
-    }
-})
-app.post("/login",async(req,res)=>{
-    try {
-        const {email,password}=req.body;
-        if(!(email && password)){
-            res.status(400).send('send all data');
-        }
-        const user = await User.findOne({email});
-
-        if(!user){
-            res.send("user doesn't exist");
-        }
-        if(user && user.IspasswordCorrect(password)){
-            const token=user.RefreshAccessToken();
-            user.token=token;
-            user.password=undefined;
-
-        // sending token in cookie
-        //cookie section
-
-        const options={
-            expires:new Date(Date.now()+3*24*3600*1000),
-            httpOnly:true,
-        }
-        res.status(200).cookie("token",token,options).json({
-            success:true,
-            token,
-            user
-        })
-
-        }
-
-
-    } catch (error) {
-        console.log(error);
-    }
-    
-})
+app.use("/register",userRouter)
+app.use("/login",userRouter)
+app.use("/",userRouter)
 
 export {app,port}
--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,10 +1,112 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.model.js";
+import {ApiResponse} from "../utils/ApiResponse.js"
 
-const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
+const registerUser = asyncHandler(async(req,res)=>{
+    try{
+        const {username,email,password}=req.body;
+        if(!(username && email && password)){
+            res.status(400).send("All fields are compulsory");
+        }
+        // Checking if user already exist
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            res.status(401).send('User already exist with this email');
+        }
+        const user=new User({
+            username,email,password
+        })
+        await user.save();
+
+        const token=user.generateAccessToken();
+        user.refreshToken=token;
+        user.password=undefined;
+
+        res.status(201).json(user);
+
+
+    }catch(err){
+        console.log(err);
+    }
+})
+
+const loginUser=asyncHandler(async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        if(!(email && password)){
+            throw new ApiError(400,"email and password both are required")
+        }
+        const user = await User.findOne({email});
+
+        if(!user){
+            throw new ApiError(404,"User does not exist")
+        }
+
+        if(user && user.isPasswordCorrect(password)){
+            const refreshtoken=user.RefreshAccessToken();
+            const accesstoken =user.generateAccessToken();
+            user.refreshToken=refreshtoken;
+
+            await user.save();
+            user.password=undefined;
+            user.refreshToken=undefined;
+        // sending token in cookie
+        //cookie section
+
+        const options={
+            httpOnly:true,
+            secure:true
+        }
+        res
+        .status(200)
+        .cookie("refreshtoken",refreshtoken,options)
+        .cookie("accesstoken",accesstoken,options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: user,accesstoken,refreshtoken
+                },
+                "User logged In successfully"
+            )
+        )
+        }
+        else{
+            throw new ApiError(401,"Password is incorrect")
+        }
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+const logoutUser=asyncHandler(async(req,res)=>{
+    try {
+        await User.findByIdAndUpdate(
+            req.user._id,
+            {
+                $set:{
+                    refreshToken:undefined
+                }
+            },
+            {
+                new:true
+            }
+        )
+        const options={
+            httpOnly:true,
+            secure:true
+        }
+    
+        return res
+        .status(200)
+        .clearCookie("accesstoken",options)
+        .clearCookie("refreshtoken",options)
+        .json(new ApiResponse(200,{},"User logged Out"))   
+    } catch (error) {
+        throw new ApiError(401,"error: ",error)
+    } 
 })
 
 
-export {registerUser};
+export {registerUser,loginUser,logoutUser};
