fe4d2dad6eec4cd9fad0286760dc86c1d79fee36
Vallabh Sangvikar <vallabhdsangvikar18@gmail.com>
2024-08-27 19:54:17 +0530

    Config cookie-js in frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
@@ -9,7 +10,7 @@ import image3 from '../assets/slider3.jpg';
 import image4 from '../assets/slider4.png';
 import './Home.css'
 import { useEffect } from 'react'
-
+import Cookies from 'js-cookie'
 import '../App.css'
 import ProductCard from '../components/Productcard';
 import demoimage from "../assets/images/demoimage.jpeg"
@@ -34,31 +35,56 @@ const productData = {
   currentPrice: 49.00,
   originalPrice: 65.00
 };
-
 function Home() {
+  const navigate = useNavigate();
   useEffect(() => {
     // Scroll to the top of the page on component mount
     window.scrollTo(0, 0);
-  }, []);
-    return (
-      <>
-        <Header />
-        <Navbar />
-        <Slider slides={slides} />
-        <br />
-        <div className='allcards'>
-          {
-            Array.from({ length: 10 }).map((_, index) => (
-              <React.Fragment key={index}>
-                <ProductCard {...productData} />  
-              </React.Fragment>
-            ))
-          }
-        </div>
-        <br />
-        <Footer />
-      </>
-    )
+  }, [navigate]);
+
+  const slides = [
+    {
+      mainImage: image1,
+    },
+    {
+      mainImage: image2,
+    },
+    {
+      mainImage: image3,
+    },
+    {
+      mainImage: image4,
+    },
+  ];
+
+  const productData = {
+    imageUrl: demoimage,
+    title: "Haldiram's",
+    description: "best bhujia due to a best website",
+    currentPrice: 49.00,
+    originalPrice: 65.00
+  };
+
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Slider slides={slides} />
+      <br />
+      <div className='allcards'>
+        {
+          Array.from({ length: 10 }).map((_, index) => (
+            <React.Fragment key={index}>
+              <ProductCard {...productData} />
+            </React.Fragment>
+          ))
+        }
+      </div>
+      <br />
+      <Footer />
+    </>
+  )
 }
 
-export default Home
+export default Home;
+  
