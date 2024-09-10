9cf193de1737f6a0831ea00472ec182ee8e8a93f
Vallabh Sangvikar <vallabhdsangvikar18@gmail.com>
2024-09-11 01:08:34 +0530

    solving img multer cloudinary error

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,8 +1,13 @@
 import {Router} from "express";
 import {createProduct,getProducts,getProductById,deleteProduct,updateProduct,getProductsByCategory} from "../controllers/admin.controller.js";
 const router = Router();
+import { upload } from "../middlewares/multer.middleware.js";
 
-router.route("/create").post( createProduct);
+router.route("/create").post( 
+    upload.fields([  // this is middleware of multer to upload files
+        { name: 'productImage', maxCount: 1 }
+    ]),
+    createProduct);
 router.route("/getproduct").get( getProducts);
 router.route("/getbyid/:id").get( getProductById);
 router.route("/delete").delete( deleteProduct);
