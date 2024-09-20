19f51320718fc594473ede29890fc14e811529b9
Vallabh Sangvikar <vallabhdsangvikar18@gmail.com>
2024-09-21 02:14:47 +0530

    setting up nodemailer and twilio

--- backend/src/routes/sendOtp.routes.js
+++ backend/src/routes/sendOtp.routes.js
@@ -3,15 +3,20 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import { Admin } from "../models/admin.model.js";
 import {Router} from "express";
+import { generateOTP,sendOTPEmail,sendOTPSMS } from "../controllers/otp.controller.js";
 
 const router = Router();
-const otp1=555555;
-const otp2=666666;
-router.route("/sendotp").get(asyncHandler(async(req,res)=>{
+
+router.route("/sendotp").post(asyncHandler(
+    async(req,res)=>{
     if(req.body.email && req.body.number){
+        const otp1 = generateOTP();
+        await sendOTPSMS(req.body.number,otp1);
         res.status(237).json(new ApiResponse(237,otp1,"OTP sent on number successfully"));
     }
     else if(req.body.email && !req.body.number){
+        const otp2 = generateOTP();
+        await sendOTPEmail(req.body.email,otp2);
         res.status(244).json(new ApiResponse(244,otp2,"OTP sent on email successfully"));
     }
     else{
